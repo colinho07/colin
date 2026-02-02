69dc7ab2585b32cd23a3d3062acae5e797f42ea4
Colin <cqrpo@mail.com>
2026-02-02 16:46:33 +0100

    urkl

--- portfolio-details-creative-light.php
+++ portfolio-details-creative-light.php
@@ -4,7 +4,7 @@
 <head>
    <meta charset="utf-8">
    <meta http-equiv="x-ua-compatible" content="ie=edge">
-   <title>PORTFOLIO - Projet 1</title>
+   <title>PORTFOLIO - Projets Montage Photo & Vidéo : LE BAR ET VOUS</title>
    <meta name="description" content="">
    <meta name="viewport" content="width=device-width, initial-scale=1">
 
