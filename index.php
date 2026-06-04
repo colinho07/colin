5ac6e720d7d9f10108c47df5a9ab7631f0dbb397
Colin <cqrpo@mail.com>
2026-06-04 15:00:35 +0200

    msg

--- index.php
+++ index.php
@@ -382,7 +382,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                            </div>
                            <div class="tp-rounded-btn-wrap tp-about-wd-btn tp_fade_anim" data-delay=".5" data-fade-from="top" data-ease="bounce">
                               <div class="btn_wrapper d-inline-block">
-                                 <a href="portfolio-col-4-light.html" class="tp-btn-rounded btn-item">
+                                 <a href="portfolio-col-3-light.php" class="tp-btn-rounded btn-item">
                                     <span class="d-block mb-10">
                                        <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                                           <path fill-rule="evenodd" clip-rule="evenodd" d="M11.3791 3.0269C14.6431 2.80336 18.8916 1.42595 21.9998 0C20.5732 3.10763 19.1953 7.35556 18.9723 10.6196L16.8276 6.04382L1.05193 21.82C0.936264 21.9354 0.779526 22.0001 0.616152 22C0.494263 22 0.375118 21.9638 0.273781 21.8961C0.172441 21.8284 0.0934544 21.7321 0.046814 21.6195C0.000171661 21.5069 -0.0120335 21.383 0.0117397 21.2634C0.035511 21.1439 0.0941944 21.034 0.18037 20.9478L15.956 5.17221L11.3791 3.0269Z" fill="currentColor" />
